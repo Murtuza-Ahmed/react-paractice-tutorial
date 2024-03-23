815a864f3882c37d27929e5bd4c03454b52330ae
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-24 02:40:13 +0500

    paractice

--- react-portfolio-tutorial/src/App.jsx
+++ react-portfolio-tutorial/src/App.jsx
@@ -1,5 +1,7 @@
+import styles from "./assets/style/App.module.css";
+
 import React from "react";
 const App = () => {
-  return <div>App</div>;
+  return <div className="App">App</div>;
 };
 export default App;
