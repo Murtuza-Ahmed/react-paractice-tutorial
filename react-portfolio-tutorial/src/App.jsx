7e6d542703aae7bb02feb79a8ef864f3cedfb7de
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-24 13:09:44 +0500

    new component hero and start

--- react-portfolio-tutorial/src/App.jsx
+++ react-portfolio-tutorial/src/App.jsx
@@ -1,11 +1,13 @@
 import styles from "./assets/style/App.module.css";
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
     <div className={styles.App}>
       <Navbar />
+      <Hero />
     </div>
   );
 };
---
+++ react-portfolio-tutorial/src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section>
+      <div>
+        <h1>Hi, I'm Murtuza</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
