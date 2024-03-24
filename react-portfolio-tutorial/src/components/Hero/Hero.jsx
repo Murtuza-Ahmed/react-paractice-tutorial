d1a10b08172decb35c6017f8a33473e02f4a6745
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-24 16:18:50 +0500

    hero section work

--- react-portfolio-tutorial/src/components/Hero/Hero.jsx
+++ react-portfolio-tutorial/src/components/Hero/Hero.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import Men from "../../assets/image/hero/men-icon.png";
+import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Hi, I'm Murtuza</h1>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Murtuza</h1>
+        <p className={styles.description}>
+          👨‍💻As a fresh web developer with a passion for designing and developing
+          user interfaces, 🎓I am currently expanding my skill set to include
+          ReactJS. My proficiency in HTML, CSS, Bootstrap, and JavaScript has
+          allowed me to create visually appealing and highly functional web
+          applications. I am excited about the opportunity to contribute my
+          skills, along with the ongoing learning of ReactJS, to deliver
+          outstanding web solutions that meet user needs and drive business
+          growth.
+        </p>
+        <a
+          href="mailto:murtuza.programmer@gmail.com"
+          className={styles.contactBotton}
+        >
+          Contact Me
+        </a>
       </div>
+      <img src={Men} alt="Pcture" className={styles.heroImage} />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
   );
 };
