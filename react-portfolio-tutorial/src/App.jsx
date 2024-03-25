c656f1835b18aa1e5a5376852b73ce09f19d6ac4
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-25 16:28:08 +0500

    new component about and start work and App.jsx call About component

--- react-portfolio-tutorial/src/App.jsx
+++ react-portfolio-tutorial/src/App.jsx
@@ -2,12 +2,14 @@ import styles from "./assets/style/App.module.css";
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 
 const App = () => {
   return (
     <div className={styles.App}>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 };
---
+++ react-portfolio-tutorial/src/components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import WomenLaptop from "../../assets/image/about/3d-woman-laptop.png";
+
+const About = () => {
+  return (
+    <section>
+      <h2>About</h2>
+      <div>
+        <img src={WomenLaptop} alt="Siting With Laptop" />
+      </div>
+    </section>
+  );
+};
+export default About;
