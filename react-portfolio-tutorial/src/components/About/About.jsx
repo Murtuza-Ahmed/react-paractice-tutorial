86b0796c3c70c61490244175b2d433bd36dfa179
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-25 17:16:44 +0500

    image change

--- react-portfolio-tutorial/src/components/About/About.jsx
+++ react-portfolio-tutorial/src/components/About/About.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./About.module.css";
 import WomenLaptop from "../../assets/image/about/3d-woman-laptop.png";
 import Cursor from "../../assets/image/about/arrow.png";
+import MUI from "../../assets/image/about/laptop.png";
+import Backend from "../../assets/image/about/mobile.png";
 
 const About = () => {
   return (
@@ -22,6 +24,12 @@ const About = () => {
                 I'm frontend developer with experience in build and optimized
                 sites
               </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={Backend} alt="CPU-Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>BackEnd Developer</h3>
             </div>
           </li>
         </ul>
