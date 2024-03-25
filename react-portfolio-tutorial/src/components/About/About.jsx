9e94a8f4941d13299fd2b6cc85e51853d2f6ff6f
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-25 16:42:40 +0500

    about end and style work

--- react-portfolio-tutorial/src/components/About/About.jsx
+++ react-portfolio-tutorial/src/components/About/About.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import styles from "./About.module.css";
 import WomenLaptop from "../../assets/image/about/3d-woman-laptop.png";
+import Cursor from "../../assets/image/about/arrow.png";
 
 const About = () => {
   return (
-    <section>
-      <h2>About</h2>
-      <div>
-        <img src={WomenLaptop} alt="Siting With Laptop" />
+    <section className={styles.container}>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={WomenLaptop}
+          alt="Siting With Laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={Cursor} alt="Cursor-Icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm frontend developer with experience in build and optimized
+                sites
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   );
