5b6e2dc74fb4661ece90b79a45689856b753fbd0
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-26 16:11:34 +0500

    solve the problem

--- react-portfolio-tutorial/src/components/Experience/Experience.jsx
+++ react-portfolio-tutorial/src/components/Experience/Experience.jsx
@@ -1,7 +1,24 @@
+import skills from "../../data/skills.json";
+import { getImageUrl } from "../../pages/utils";
+
 const Experience = () => {
   return (
     <section id="experience">
       <h2>Experience</h2>
+      <div></div>
+      <div>
+        {skills.map((skill, id) => {
+          return (
+            <div key={id}>
+              <div>
+                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+              </div>
+              <p>{skill.title}</p>
+            </div>
+          );
+        })}
+      </div>
+      <ul></ul>
     </section>
   );
 };
