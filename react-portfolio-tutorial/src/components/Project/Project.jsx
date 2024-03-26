13f1ae500154d3c267fd4dbd0fa91a90a3bf0270
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-27 01:53:54 +0500

    project complete and project style work start

--- react-portfolio-tutorial/src/components/Project/Project.jsx
+++ react-portfolio-tutorial/src/components/Project/Project.jsx
@@ -1,30 +1,14 @@
 import styles from "./Project.module.css";
 import projects from "../../data/projects.json";
-import { getImageUrl } from "../../pages/utils";
+import ProjectCard from "./ProjectCard";
 
 const Project = () => {
   return (
-    <section>
-      <h2>Projects</h2>
-      <div>
+    <section className={styles.container}>
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.project}>
         {projects.map((project, id) => (
-          <div key={id}>
-            <img
-              src={getImageUrl(project.imageSrc)}
-              alt={`Image of ${project.title}`}
-            />
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-            <ul>
-              {project.skills.map((skill, id) => (
-                <li key={id}>{skill}</li>
-              ))}
-            </ul>
-            <div>
-              <a href={project.demo}>Demo</a>
-              <a href={project.sourcs}>Source</a>
-            </div>
-          </div>
+          <ProjectCard key={id} project={project} />
         ))}
       </div>
     </section>
