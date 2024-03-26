ecc404ec53742709eb7088364d3fd382323d9e6f
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-26 15:57:38 +0500

    experience component start

--- react-portfolio-tutorial/src/components/Experience/Experience.jsx
+++ react-portfolio-tutorial/src/components/Experience/Experience.jsx
@@ -1,25 +1,7 @@
-import skills from "../../data/skills.json";
-import { getImageUrl } from "../../pages/utils";
-console.log(getImageUrl);
-console.log(skills);
-
 const Experience = () => {
   return (
     <section id="experience">
       <h2>Experience</h2>
-      <div>
-        <div>
-          <div>
-            {skills.map((skill, id) => (
-              <div key={id}>
-                <div>
-                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
     </section>
   );
 };
