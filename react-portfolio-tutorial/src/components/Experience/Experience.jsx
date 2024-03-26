011d07550e7909094eaf4b39ae63dc075850777d
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-26 16:41:52 +0500

    experience component complete and style start

--- react-portfolio-tutorial/src/components/Experience/Experience.jsx
+++ react-portfolio-tutorial/src/components/Experience/Experience.jsx
@@ -1,31 +1,42 @@
 import skills from "../../data/skills.json";
 import historyItem from "../../data/history.json";
 import { getImageUrl } from "../../pages/utils";
+import styles from "./Experience.module.css";
 
 const Experience = () => {
   return (
-    <section id="experience">
-      <h2>Experience</h2>
-      <div></div>
-      <div>
-        {skills.map((skill, id) => {
-          return (
-            <div key={id}>
-              <div>
-                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+    <section id="experience" className={styles.container}>
+      <h2 className={styles.title}>Experience</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
+          {skills.map((skill, id) => {
+            return (
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
               </div>
-              <p>{skill.title}</p>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-      <ul>
+      <ul className={styles.history}>
         {historyItem.map((history, id) => (
-          <li key={id}>
+          <li key={id} className={historyItem}>
             <img
               src={getImageUrl(history.imageSrc)}
               alt={`${history.organisation} Logo`}
             />
+            <div className={historyItemDetail}>
+              <h3>{`${history.role}, ${history.organisation}`}</h3>
+              <p>{`${history.startDate} - ${history.endDate}`}</p>
+              <ul>
+                {history.experiences.map((exp, id) => (
+                  <li key={id}>{exp}</li>
+                ))}
+              </ul>
+            </div>
           </li>
         ))}
       </ul>
