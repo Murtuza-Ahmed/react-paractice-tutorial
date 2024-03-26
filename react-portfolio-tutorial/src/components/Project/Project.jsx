991961c99f530ad5f6e3378fae75633c97eb05ee
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-27 01:31:21 +0500

    project json file add skill and project jsx complete

--- react-portfolio-tutorial/src/components/Project/Project.jsx
+++ react-portfolio-tutorial/src/components/Project/Project.jsx
@@ -1,7 +1,33 @@
 import styles from "./Project.module.css";
 import projects from "../../data/projects.json";
+import { getImageUrl } from "../../pages/utils";
 
 const Project = () => {
-  return <div>project</div>;
+  return (
+    <section>
+      <h2>Projects</h2>
+      <div>
+        {projects.map((project, id) => (
+          <div key={id}>
+            <img
+              src={getImageUrl(project.imageSrc)}
+              alt={`Image of ${project.title}`}
+            />
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <ul>
+              {project.skills.map((skill, id) => (
+                <li key={id}>{skill}</li>
+              ))}
+            </ul>
+            <div>
+              <a href={project.demo}>Demo</a>
+              <a href={project.sourcs}>Source</a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 export default Project;
