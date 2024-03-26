75d18a00725257a92668b63465f49b6e006fb900
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-27 00:51:57 +0500

    new component project start

--- react-portfolio-tutorial/src/App.jsx
+++ react-portfolio-tutorial/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
+import Project from "./components/Project/Project";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Hero />
       <About />
       <Experience />
+      <Project />
     </div>
   );
 };
