026bdeb67a456aa789802cec8a6a9cb362a0d56b
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-27 15:54:54 +0500

    contact complete and style start

--- react-portfolio-tutorial/src/components/Contact/Contact.jsx
+++ react-portfolio-tutorial/src/components/Contact/Contact.jsx
@@ -1,4 +1,30 @@
+import { getImageUrl } from "../../pages/utils";
+import run from "../../assets/image/contact/email.png";
 const Contact = () => {
-  return <div>contact</div>;
+  return (
+    <footer>
+      <div>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul>
+        <li>
+          <img src={getImageUrl("contact/email.png")} alt="Email-Icon" />
+          <a href="http://www.murtuza.programmer@gmail.com" target="_blank">
+            MyEmail@email.com
+          </a>
+        </li>
+        <li>
+          <img src={getImageUrl("contact/linkdin.png")} alt="LinkedIn-Icon" />
+          <a href="https://www.linkedin.com/in/murtuza-ahmed-36012628b/"></a>
+          LinkedIn.com
+        </li>
+        <li>
+          <img src={getImageUrl("contact/git-hub.png")} alt="GitHub-Icon" />
+          <a href="https://github.com/Murtuza-Ahmed">GitHub.com</a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 export default Contact;
