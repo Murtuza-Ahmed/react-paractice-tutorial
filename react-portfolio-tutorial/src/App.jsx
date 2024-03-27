24b3a20a13c065b142181483ee8cf40c55deedc9
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-03-27 15:29:33 +0500

    fotter contatc section

--- react-portfolio-tutorial/src/App.jsx
+++ react-portfolio-tutorial/src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 import Project from "./components/Project/Project";
+import Contact from "./components/Contact/Contact";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <About />
       <Experience />
       <Project />
+      <Contact />
     </div>
   );
 };
