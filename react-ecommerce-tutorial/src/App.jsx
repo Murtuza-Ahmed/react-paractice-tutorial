975e7ba70388a52bf05e52b250d0c6058ade467a
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-04-10 22:25:20 +0500

    paractice

--- react-ecommerce-tutorial/src/App.jsx
+++ react-ecommerce-tutorial/src/App.jsx
@@ -1,9 +1,16 @@
 import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./pages/Shop";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
