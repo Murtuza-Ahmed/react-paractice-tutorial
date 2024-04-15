26f2df6269e06d9ca4df9cf8784fcbe29109c3cb
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-04-15 14:05:32 +0500

    work continue

--- react-ecommerce-tutorial/src/pages/ShopCategory.jsx
+++ react-ecommerce-tutorial/src/pages/ShopCategory.jsx
@@ -1,5 +1,10 @@
-const ShopCategory = () => {
-  return <div></div>;
+import { useContext } from "react";
+import styles from "./CSS/ShopCategory.module.css";
+import { ShopContext } from "../context/ShopContext";
+
+const ShopCategory = (props) => {
+  const {all_product} = useContext(ShopContext)
+  return <div className={styles.ShopCategory}></div>;
 };
 
 export default ShopCategory;
